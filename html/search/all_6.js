826a75cb2d5de1cedfa055843721bb60d520772d
Anthony Nguyen <anthony@cs.utexas.edu>
2016-07-11 21:45:17 -0500

    fixed deque.h after travis ci failed. changed inner_size to static

--- html/search/all_6.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['insert',['insert',['../classmy__deque.html#a27f7317e3da46b7683d83ff9c06b5b59',1,'my_deque']]],
-  ['iterator',['iterator',['../classmy__deque_1_1iterator.html#a9e89f763f9bc57b554854ec55ef1e29a',1,'my_deque::iterator']]],
-  ['iterator',['iterator',['../classmy__deque_1_1iterator.html',1,'my_deque']]]
-];
